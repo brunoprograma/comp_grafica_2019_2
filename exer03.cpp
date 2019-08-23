74897174e20d6f7d7e00c18d29e46f2dafd0c56e
Bruno Ribeiro <brunoribeiroinf@gmail.com>
2019-08-22 22:12:51 -0300

    Add files via upload

---
+++ exer03.cpp
@@ -0,0 +1,90 @@
+/**
+ * Basic usege of OpenGL and GLFW. 
+ */
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+#define WIDTH 500
+#define HEIGHT 500
+
+float x1 = -0.99f;
+float x2 = 0.59;
+float y1 = 0.40f;
+float sentido = 1.0f;
+
+int main() {
+	GLFWwindow* window;
+	int width, height;
+
+	if (!glfwInit()) {
+		std::cout << "Problem to initialize GLFW" << std::endl;
+		exit(1);
+	}
+	
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Hello world, GLFW!", NULL, NULL);
+
+	if (!window) {
+		std::cout << "Problem to create GLFW window" << std::endl;
+		glfwTerminate();
+		exit(1);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while(glfwWindowShouldClose(window) == 0 && glfwGetKey(window, GLFW_KEY_ESCAPE) == 0) {
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+
+		glBegin(GL_LINES);
+			if (x1 >= 0.59f) {
+				sentido = -1.f;
+			} else if (x1 <= -0.99f) {
+				sentido = 1.f;
+			}
+			x1 += 0.02f * sentido;
+			x2 = x1 * -1.0f - 0.4f;
+
+			// quadrado red
+			glColor3f(1.f, 0.f, 0.f);
+			
+			glVertex3f(x1, y1, 0.f);
+			glVertex3f(x1, y1-0.4, 0.f);
+
+			glVertex3f(x1+0.4, y1, 0.f);
+			glVertex3f(x1+0.4, y1-0.4, 0.f);
+
+			glVertex3f(x1, y1, 0.f);
+			glVertex3f(x1+0.4, y1, 0.f);
+
+			glVertex3f(x1, y1-0.4, 0.f);
+			glVertex3f(x1+0.4, y1-0.4, 0.f);
+
+			// quadrado blue
+			glColor3f(0.f, 0.f, 1.f);
+			
+			glVertex3f(x2, y1, 0.f);
+			glVertex3f(x2, y1-0.4, 0.f);
+
+			glVertex3f(x2+0.4, y1, 0.f);
+			glVertex3f(x2+0.4, y1-0.4, 0.f);
+
+			glVertex3f(x2, y1, 0.f);
+			glVertex3f(x2+0.4, y1, 0.f);
+
+			glVertex3f(x2, y1-0.4, 0.f);
+			glVertex3f(x2+0.4, y1-0.4, 0.f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+    glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
